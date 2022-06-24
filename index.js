bd6d99943bc37de7f6f03304924e65a4df7bafe5
华晨 <774031882@qq.com>
2022-06-24 09:37:58 +0800

    登录

--- index.js
+++ index.js
@@ -7,14 +7,30 @@ app.use(cors())
 app.use(express.json())
 
 const users = [
-    { id: 1, type: 2, username: '华山', name: '华晨', avatar: 'https://tva1.sinaimg.cn/large/e6c9d24egy1h36tkf2rxrj20po0q4gp4.jpg', start: '2022-04-18', level: 3, score: 8876, badges: [] },
-    { id: 2, type: 2, username: '小龙', name: '方晓龙', avatar: 'https://tva1.sinaimg.cn/large/e6c9d24egy1h36ok2cgx6j20py0q2jsy.jpg', start: '2022-04-18', level: 6, score: 66666, badges: [] },
+    { id: 1, type: 2, username: '华山', password: '123', name: '华晨', avatar: 'https://tva1.sinaimg.cn/large/e6c9d24egy1h36tkf2rxrj20po0q4gp4.jpg', start: '2022-04-18', level: 3, score: 8876, badges: [] },
+    { id: 2, type: 2, username: '小龙', password: '123', name: '方晓龙', avatar: 'https://tva1.sinaimg.cn/large/e6c9d24egy1h36ok2cgx6j20py0q2jsy.jpg', start: '2022-04-18', level: 6, score: 66666, badges: [] },
 ]
 
 const notes = [
     { id: 1, title: '学习 Express', content: '<p>反人性</p>', isPublic: true },
     { id: 2, title: '学习 Node.js', content: '</p>厉害的人反人性</p>', isPublic: false },
 ]
+
+app.post('/auth/login', (req, res) => {
+    const { username, password } = req.body
+    const index = users.findIndex(u => u.username === username && u.password === password)
+    if (index === -1) {
+        res
+            .status(400)
+            .json({
+                message: '用户名或密码错误',
+            })
+    } else {
+        const token = String(Math.random()).slice(2)
+        users[index].token = token
+        res.json({ token })
+    }
+})
 
 app.get('/users', (req, res) => {
     const page = +req.query.page || 1
